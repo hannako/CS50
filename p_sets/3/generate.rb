79e03780f0a7c83f28fe031e92833e5092bc142e
hannako <jessjones1@gmail.com>
2016-12-16 15:18:41 +0000

    random number generator

---
+++ p_sets/3/generate.rb
@@ -0,0 +1,31 @@
+class RandomArrayGenerator
+  EXPONENT = 10**3
+  attr_reader :length, :seed
+
+  def initialize(length, seed = nil)
+    @length = length
+    @seed = seed
+  end
+
+  def array
+    Array.new(length) { (rand * EXPONENT).ceil }
+  end
+
+  def seed_rand
+    srand seed
+  end
+end
+
+def main
+  if ARGV.count > 2 || ARGV.empty?
+    exit 1
+  else
+    length, *seed = ARGV.map(&:to_i)
+    generate = RandomArrayGenerator.new(length, seed.first)
+    unless seed.empty? then generate.seed_rand
+    end
+    puts generate.array
+  end
+end
+
+main
