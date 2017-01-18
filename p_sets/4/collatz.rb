8b2c6ad4c4a885771d6e12eb5f6d3803aa1791a5
hannako <jessjones1@gmail.com>
2017-01-18 17:01:12 +0000

    replaces ternary with if/else

--- p_sets/4/collatz.rb
+++ p_sets/4/collatz.rb
@@ -3,6 +3,10 @@ module Collatz
     count ||= 0
     return count if int == 1
     count += 1
-    int.even? ? steps(int / 2, count) : steps((3 * int + 1), count)
+    if int.even?
+      steps(int / 2, count)
+    else
+      steps((3 * int + 1), count)
+    end
   end
 end
